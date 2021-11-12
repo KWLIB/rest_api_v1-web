4d708624127a1fd161f8a3e5048f825f7cad59ae
Karen Wu <qwu334@gmail.com>
2021-11-12 13:00:48 -0500

    create website

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Introduction
+creating a website using express
+
+## setup
+1. install express generator
+-npm install express-generator -g
+2. create new web using generator
+-express restaurantwebs --view=hbs
+
+# npm install
+# npm start
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const { response } = require('express');
+var express = require('express');
+var router = express.Router();
+const apiHelper = require('../helper/apiHelper');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  apiHelper.callApi('http://localhost:5000/api').then(response=>{
+    console.log(response);
+    res.render('index',{title:'Restaurant Evaluation', data: response.data});
+  }).catch(error=>{
+    res.send(error);
+  })
+})
+
+module.exports = router;
